df4a8b4064c68b3923a0117d2bb4e84b1424d692
dmm <moshenskydenis@gmail.com>
2018-09-26 21:50:46 +0300

    major bugs fixed

--- markmods/aln/seqs/probcons.py
+++ markmods/aln/seqs/probcons.py
@@ -9,13 +9,13 @@ from markmods.models.hmm.hmm import HMM
 from markmods.models.hmm.hmm_profile import hmm_profile
 
 
-class HubsTree:
+class HubsTree(object):
     def __init__ (self, hubs, levels):
         self.hubs = hubs
         self.levels = levels
 
 
-class Hub:
+class Hub(object):
     def __init__ (self, content, links):
         self.content = content
         self.links = links
@@ -53,8 +53,9 @@ def probcons(sequences, emis, threads, delta, n_iter, match, match_to_match, gap
     mult_alignment = mult_from_pairs(pair_alignments, sequences, hmm, p_match_c, l_bonus, dist_step, threads)
     subprocess.call("redis-cli shutdown", shell=True)
     return mult_alignment[0], mult_alignment[1], pair_alignments[1], emis_new
-###
-def freqs_calc (ss):
+
+
+def freqs_calc(ss):
     s_nums = {}
     s_num = 0
     s_k = ss.keys()
@@ -68,52 +69,60 @@ def freqs_calc (ss):
     s_nums = num_to_freq(s_nums, s_num)
     s_nums = pairs_freq(s_nums)
     return s_nums
-###
-def num_to_freq (nums_d, num):
+
+
+def num_to_freq(nums_d, num):
     n_k = nums_d.keys()
     for k in n_k:
         nums_d[k] = float(nums_d[k])/float(num)
     return nums_d
-###
-def pairs_freq (freqs_d):
+
+
+def pairs_freq(freqs_d):
     pairs_fr = {}
     f_k = freqs_d.keys()
     for ki in f_k:
         for kj in f_k:
             pairs_fr[frozenset([ki,kj])] = freqs_d[ki]*freqs_d[kj]
     return pairs_fr
-###
-def test_print (f, struc):
+
+
+def test_print(f, struc):
     w = open(f, 'w')
     w.write(pickle.dumps(struc))
     w.close()    
-###
-def log_matrix (matr):
+
+
+def log_matrix(matr):
     log_matr = {}
     m_k = matr.keys()
     for k in m_k:
         log_matr[k] = log(matr[k])
     return log_matr
-###
-def get_params_log (*par_l):
+
+
+def get_params_log(*par_l):
     log_par = []
     for p in par_l:
         log_par.append(log(p))
     return log_par
-###
-def params_calc (d, it):
+
+
+def params_calc(d, it):
     k = 1.0
     c = 1.0/d - (k*sqrt(it)+1.0)
     while c > 5.0:
         k = k + 1.0
         c = 1.0/d - (k*sqrt(it)+1.0)
     return c, k
-###
-def temp_calc (par, i):
+
+
+def temp_calc(par, i):
     t = par[1]*sqrt(i)+par[0]
     return t
-###
-def get_seq_pairs (seqs):
+
+
+def get_seq_pairs(seqs):
     i = 0
     keys_l = seqs.keys()
     l = len(keys_l)
@@ -125,8 +134,9 @@ def get_seq_pairs (seqs):
             j = j + 1
         i = i + 1
     return s_pairs
-###
-def get_pair_alignments (s_pairs, hmm, l_bon, n_thr):
+
+
+def get_pair_alignments(s_pairs, hmm, l_bon, n_thr):
     pair_alns = {}
     probs = {}
     run_list = []
@@ -147,8 +157,9 @@ def get_pair_alignments (s_pairs, hmm, l_bon, n_thr):
         probs[pickle.loads(k)] = pickle.loads(r.get(k))
     r.flushdb()    
     return pair_alns, probs
-###
-def align_pair (run_list):
+
+
+def align_pair(run_list):
     pair = run_list[0]
     hmm = run_list[1]
     l_b = run_list[2]
@@ -162,8 +173,9 @@ def align_pair (run_list):
     r.append(p_aln_k, pickle.dumps(pair_aln))
     r = redis.StrictRedis(host='localhost', db=db2)
     r.append(p_aln_k, pickle.dumps(float(p_states_seq[1])))
-###
-def build_alignment (st_seq, s_pair):
+
+
+def build_alignment(st_seq, s_pair):
     k = s_pair.keys()
     seq1 = []
     seq2 = []
@@ -188,8 +200,9 @@ def build_alignment (st_seq, s_pair):
             seq2.append(s_pair[k[1]][pos[1]-1])
         sts.append(pos[0])
     return {k[0]:seq1, k[1]:seq2, "states":sts}
-###
-def emis_calc (pair_alns, e_d, symb_fr, t):
+
+
+def emis_calc(pair_alns, e_d, symb_fr, t):
     num_dict = {}
     subs_dict = {}
     pa_k = pair_alns.keys()
@@ -226,8 +239,9 @@ def emis_calc (pair_alns, e_d, symb_fr, t):
             n = n + 1
     emis_probs = subs_calc(subs_dict, symb_fr, num_dict, e_d, t)
     return emis_probs
-###
-def keys_sort (k_l):
+
+
+def keys_sort(k_l):
     n = 0
     for k in k_l:
        if k == "states":
@@ -235,34 +249,37 @@ def keys_sort (k_l):
            k_l.append(k)
            return k_l
        n = n + 1
-###
-def subs_calc (s_dict, fr_dict, n_dict, e, t):
+
+
+def subs_calc(s_dict, fr_dict, n_dict, e, t):
     for key in e.keys():
         try:
             e[key] = (t*e[key]+(float(s_dict[key])/fr_dict[key])/float(n_dict[key]))/(t+1)
         except KeyError:
             e[key] = t*e[key]/(t+1)
     return e
-###
-def sum_matrix (m):
+
+
+def sum_matrix(m):
 #the matrix is dictionary!
     s = 0.0
     m_k = m.keys()
     for k in m_k:
         s = s + m[k]
     return s
-###
-def mult_from_pairs (pair_alns, seqs, hmm, match_c, l_bon, d_step, n_thr):
+
+
+def mult_from_pairs(pair_alns, seqs, hmm, match_c, l_bon, d_step, n_thr):
     s_k = seqs.keys()
-    print "Guide tree building" 
+    print("Guide tree building")
 #    order_list = komiv(pair_alns[1], s_k[0])#the correct way to go through is NP problem (komiv)
     order_list = s_k#
     hubs_tree = select_clust(order_list, pair_alns[1], d_step)#should be inserted to komiv
     lev_num = len(hubs_tree.levels)
     n = -1
-    print "Multiple alignment building"
+    print("Multiple alignment building")
     while n+lev_num >= 0:
-        print "level %s" % abs(n)
+        print("level %s" % abs(n))
         run_list = configure_run(hubs_tree.levels[n], hubs_tree.hubs, seqs, hmm, match_c, l_bon, 0)
         pool = mp.Pool(n_thr)
         result = pool.map(hubs_aln, run_list)
@@ -274,14 +291,16 @@ def mult_from_pairs (pair_alns, seqs, hmm, match_c, l_bon, d_step, n_thr):
     mult_aln = hubs_tree.hubs[aln_key].content
     mult_aln_profile = hmm_profile(mult_aln)
     return mult_aln, mult_aln_profile
-###
-def configure_run (levs, hs, ss, hmm, m_c, l_b, db):
+
+
+def configure_run(levs, hs, ss, hmm, m_c, l_b, db):
     r_list = []
     for k in levs:
         r_list.append([k, hs, ss, hmm, m_c, l_b, db])
     return r_list
-###
-def hubs_aln (r_list):
+
+
+def hubs_aln(r_list):
     hub_k = r_list[0]
     hubs = r_list[1]
     seqs = r_list[2]
@@ -308,8 +327,9 @@ def hubs_aln (r_list):
         aln1 = build_mult_aln(aln1_states[0], aln1, aln2)
     r = redis.StrictRedis(host='localhost', db=db)
     r.append(pickle.dumps(hub_k), pickle.dumps(aln1))
-###
-def select_clust (ord_l, dist, d_st):
+
+
+def select_clust(ord_l, dist, d_st):
     mi_dist = min_dist(dist)
     rmi_dist = round(mi_dist, 0)#this works only with d_st = -0.5
     if rmi_dist > mi_dist:
@@ -326,20 +346,22 @@ def select_clust (ord_l, dist, d_st):
         splits.append(clust_split)
         clust_split = clust_split + d_st
     split_dists = define_split_dist(cl_num_f, splits)
-    print "%s levels in tree" % len(split_dists)
+    print("%s levels in tree" % len(split_dists))
     ord_l = cor_start(ord_l, dist, split_dists[0])
     lev0 = frozenset(ord_l)
     return define_hubs(lev0, dist, split_dists, 1, HubsTree({lev0:Hub(ord_l, (ord_l[0], ord_l[-1]))}, [[lev0]]))
-###
-def min_dist (d):
+
+
+def min_dist(d):
     d_k = d.keys()
     min_d = d[d_k[0]]
     for k in d_k:
         if d[k] > min_d:#depends on distances signs
             min_d = d[k]
     return min_d
-###
-def cl_num_calc (o_l, d, cl_spl):
+
+
+def cl_num_calc(o_l, d, cl_spl):
     cl_n = 0
     n = 0
     l = len(o_l)
@@ -354,8 +376,9 @@ def cl_num_calc (o_l, d, cl_spl):
     if cl_n == 0:
         cl_n = 1
     return cl_n
-###
-def define_split_dist (f, x):
+
+
+def define_split_dist(f, x):
     spls = []
     f_1 = num_deriv(f, x)
     f_2 = num_deriv(f_1, x)
@@ -366,23 +389,26 @@ def define_split_dist (f, x):
                 spls.insert(0, xi)
     spls.insert(0, x[-1])
     return spls
-###
-def define_loc_max (f, x):
+
+
+def define_loc_max(f, x):
     max_l = []
     for n in range(1, len(x)-1):
         if f[x[n]] > f[x[n-1]] and f[x[n]] > f[x[n+1]]:
             max_l.append(x[n])
     return max_l
-###
-def func_par_wr (f_name, f, x):
+
+
+def func_par_wr(f_name, f, x):
     w = open(f_name+"y.txt", 'w')
     w.write(pickle.dumps(f))
     w.close()
     w = open(f_name+"x.txt", 'w')
     w.write(pickle.dumps(x))
     w.close()    
-###
-def num_deriv (f, x):
+
+
+def num_deriv(f, x):
     deriv = {x[0]:float(f[x[1]]-f[x[0]])/float(x[1]-x[0])}
     n = 1
     l_x = len(x)
@@ -393,8 +419,9 @@ def num_deriv (f, x):
             deriv[x[n]] = float(f[x[n]]-f[x[n-1]])/float(x[n]-x[n-1])
         n = n + 1
     return deriv
-###
-def cor_start (o_l, d, spl):
+
+
+def cor_start(o_l, d, spl):
     n = 0
     l = len(o_l)
     while l+n > 0:
@@ -406,8 +433,9 @@ def cor_start (o_l, d, spl):
             break
         n = n - 1
     return o_l
-###
-def define_hubs (h_key, d, spls, lev, h_tree):
+
+
+def define_hubs(h_key, d, spls, lev, h_tree):
     o_l = h_tree.hubs[h_key].content
     try:
         cl_spl = spls[lev]
@@ -435,8 +463,9 @@ def define_hubs (h_key, d, spls, lev, h_tree):
     for i in range(0,len(branch_hubs_k)):
         h_tree = define_hubs(branch_hubs_k[i], d, spls, lev+1, h_tree)
     return hubs_sort(h_key, h_tree, branch_hubs_k, d)
-###
-def get_hubs_keys (b_hs, hs):
+
+
+def get_hubs_keys(b_hs, hs):
     b_hs_k = []
     for h in b_hs:
         if str(type(h)) == "<type 'list'>":
@@ -452,8 +481,9 @@ def get_hubs_keys (b_hs, hs):
         b_hs_k.append(h_k)
         h_k = None
     return b_hs_k, hs
-###
-def hubs_sort (h_k, h_t, b_hk, d):
+
+
+def hubs_sort(h_k, h_t, b_hk, d):
     h = h_t.hubs
     l = len(b_hk)
     if l == 1:
@@ -473,8 +503,9 @@ def hubs_sort (h_k, h_t, b_hk, d):
         k = None
     h_t.hubs[h_k].content = get_hub_order(h, b_hk, min_e, d)
     return h_t
-###
-def get_hub_order (h, b_hk, st_pair, d):
+
+
+def get_hub_order(h, b_hk, st_pair, d):
     to_prev = None
     to_next = None
     s_h = []
@@ -499,8 +530,9 @@ def get_hub_order (h, b_hk, st_pair, d):
             to_next = None
             n = n + 1
     return s_h        
-###
-def select_base (p_alns, o_list, ss):
+
+
+def select_base(p_alns, o_list, ss):
     p_alns_l = []
     n = 0
     l_o_list = len(o_list)
@@ -511,16 +543,18 @@ def select_base (p_alns, o_list, ss):
             p_alns_l.append(p_alns[frozenset([o_list[n], o_list[n+1]])])
         n = n + 2
     return p_alns_l
-###
-def read_db (db, hubs_d):
+
+
+def read_db(db, hubs_d):
     r = redis.StrictRedis(host='localhost', db=db)
     r_k = r.keys('*')
     for k in r_k:
         hubs_d[pickle.loads(k)].content = pickle.loads(r.get(k))
     r.flushdb()
     return hubs_d    
-###
-def build_mult_aln (st_seq, aln1, aln2):
+
+
+def build_mult_aln(st_seq, aln1, aln2):
     aln1_k = aln1.keys()
     aln2_k = aln2.keys()
     aln = aln_prepare(aln1_k, aln2_k)
@@ -542,31 +576,36 @@ def build_mult_aln (st_seq, aln1, aln2):
             aln = append_gap(aln, aln1, aln1_k)
             aln = append_let(aln, aln2, aln2_k, pos[1]-1)
     return aln
-###
-def aln_prepare (a1_k, a2_k):
+
+
+def aln_prepare(a1_k, a2_k):
     aln = {}
     for k in a1_k:
          aln[k] = []
     for k in a2_k:
          aln[k] = []
     return aln
-###
-def append_let (aln, a, a_k, p):
+
+
+def append_let(aln, a, a_k, p):
     for k in a_k:
         aln[k].append(a[k][p])
     return aln
-###
-def append_gap (aln, a, a_k):
+
+
+def append_gap(aln, a, a_k):
     for k in a_k:
         aln[k].append("-")
     return aln
-###
-def log_writer (s, si):
+
+
+def log_writer(s, si):
     w = open("log.txt", 'ab')
     w.write(s.encode('cp1251')+"\t"+si+"\n")
     w.close()
-###
-def matrix_writer (f, matr):
+
+
+def matrix_writer(f, matr):
     m_dict = {}
     m_k = matr.keys()
     for k in m_k:
@@ -591,8 +630,9 @@ def matrix_writer (f, matr):
         k_k.sort()
         for ki in k_k:
             writer(f, k+"-"+ki+":"+str(m_dict[k][ki]))
-###
-def writer (f, s):
+
+
+def writer(f, s):
     w = open (f, "a")
     w.write(s + "\n")
     w.close()

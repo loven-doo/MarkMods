d9eeb0ebcad1301038a6e2ad14ca5229ae8737e4
DenisMoshenskiy <Denis.Moshenskiy@allianz.ru>
2018-11-23 20:19:32 +0300

    aggr idea added

--- markmods/models/base.py
+++ markmods/models/base.py
@@ -19,3 +19,30 @@ class ModelBase(metaclass=ABCMeta):
     @abstractmethod
     def load(cls, scheme_path):
         pass
+
+
+def aggregate_array(array, aggr_level, aggr_lims=None):
+    if aggr_level < 1:
+        return array
+    aggr_array = list()
+    if aggr_level == 1:
+        for array_elm in array:
+            for elm in array_elm:
+                if type(elm) not in (int, bool, float):###
+                    if type(elm) is dict:
+                        for feature in elm:
+                            if type(elm[feature]) not in (int, bool, float):
+                                bill_rec_logger.info(elm)
+                    else:
+                        bill_rec_logger.info(elm)###
+            aggr_array.extend(array_elm)
+    else:
+        for array_elm in array:
+            aggr_array.extend(aggregate_array(array=array_elm, aggr_level=aggr_level-1))
+    return aggr_array
+
+
+def group_array(aggr_array, aggr_lims):
+
+    return
+
